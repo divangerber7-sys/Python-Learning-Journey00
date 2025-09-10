22fa563055357b099dc537429571b1d4946f8c07
divangerber7-sys <divangerber7@gmail.com>
2025-09-10 19:41:43 +0200

    Create type_casting.py

---
+++ type_casting/type_casting.py
@@ -0,0 +1,16 @@
+
+## Code
+
+```python
+# (string, integer, float, boolean)
+# Explicit vs Implicit
+
+# Explicit Typecasting - Manually converting to another
+
+name = "Bro"
+age = 21
+gpa = 1.9
+student = True
+
+name = bool(name)
+print(name)
