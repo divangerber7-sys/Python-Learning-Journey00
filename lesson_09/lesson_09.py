9b5e83e08e0739c390e0083713d80462d9281959
divangerber7-sys <divangerber7@gmail.com>
2025-09-10 18:57:07 +0200

    Create lesson_09.py

---
+++ lesson_09/lesson_09.py
@@ -0,0 +1,39 @@
+
+---
+
+### **lesson_09/lesson_09.py**
+
+```python
+# Logical operators = used on conditional statements
+
+#       and = checks two or more conditions if True
+#        or = checks if at least one condition is True
+#       not = True if condition is False, and vice versa
+
+# Example 1: using `and`
+"""
+temp = 25
+
+if temp > 0 and temp < 30:
+    print("This is a good temperature!!!")
+else:
+    print("This is a bad temperature!!!")
+"""
+
+# Example 2: using `or`
+"""
+temp = 40
+
+if temp <= 0 or temp >=30:
+    print("This is a bad temperature!!!")
+else:
+    print("This is a good temperature!!!")
+"""
+
+# Example 3: using `not`
+sunny = True
+
+if not sunny:
+    print("This is a sunny day!!!")
+else:
+    print("This is a cloudy day!!!")
